9bd6861c171e6bc676540e668a22ea62c9edeeb1
evan2x <aiweizhang@creditease.cn>
2015-10-04 08:18:56 +0800

    add koa-compress middleware

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
 var app = require('koa')();
 var serve = require('koa-static');
 var logger = require('koa-logger');
+var compress = require('koa-compress');
 
+app.use(compress());
 app.use(logger());
 app.use(serve('.'));
 app.use(serve('./views'));
