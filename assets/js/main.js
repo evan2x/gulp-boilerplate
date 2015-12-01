3ea3ebc498b2d4869edf18a9bac8375198cc9c89
evan2x <evan2zaw@gmail.com,aiweizhang@creditease.cn>
2015-12-02 00:46:20 +0800

    Indent of 2 spaces

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,25 +5,25 @@ import {mixin} from './lib/decorators';
 @mixin(EventEmitter.prototype)
 class Person {
 
-    constructor(prop = {}) {
-        this.name = prop.name;
-        this.age = prop.age;
-    }
+  constructor(prop = {}) {
+    this.name = prop.name;
+    this.age = prop.age;
+  }
 
-    say(message){
-        this.emit('say', `${this.name} say: ${message}`);
-    }
+  say(message){
+    this.emit('say', `${this.name} say: ${message}`);
+  }
 }
 
 let evan = new Person({
-    name: 'evan',
-    age: 22
+  name: 'evan',
+  age: 22
 });
 
 evan.on('say', function(msg){
-    /* eslint-disable */
-    console.log(`${msg} \n${' '.repeat(msg.length - 4)}----by 'say' event`);
-    /* eslint-enable */
+  /* eslint-disable */
+  console.log(`${msg} \n${' '.repeat(msg.length - 4)}----by 'say' event`);
+  /* eslint-enable */
 });
 
 evan.say('Hello!');
