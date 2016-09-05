d787006d428fca744733cabfae0761134f76ca16
evan2x <evan2zaw@gmail.com,aiweizhang@creditease.cn>
2016-09-05 13:04:03 +0800

    Update eslint config

--- build/.eslintrc.js
+++ build/.eslintrc.js
@@ -1,25 +1,18 @@
 
+const OFF = 0;
 const WARN = 1;
 
 module.exports = {
-  parserOptions: {
-    ecmaVersion: 6,
-    sourceType: 'module',
-    ecmaFeatures: {
-      experimentalObjectRestSpread: true
-    }
-  },
-
   rules: {
-    'no-console': 0,
+    'no-console': OFF,
     'max-len': [WARN, 120, 2]
   },
-
   env: {
     node: true,
     browser: false,
     es6: true,
     jquery: false,
+    serviceworker: false,
     mocha: false,
   }
 }
