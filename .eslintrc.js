bf61e323acf441adb78cbc952612f9b6b57dee31
Evan <evan2zaw@gmail.com,aiweizhang@creditease.cn>
2018-09-25 11:51:25 +0800

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,30 @@
-
 const OFF = 0;
 const WARN = 1;
 const ERROR = 2;
 
 module.exports = {
-  parser: 'babel-eslint',
-  extends: 'airbnb',
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2017,
+    sourceType: 'module'
+  },
+  extends: ['airbnb-base', 'plugin:vue/essential'],
   rules: {
+    "no-unused-vars": [2, {
+      "vars": "local",
+      "args": "none"
+    }],
+    "react-in-jsx-scope":OFF,
+    "jsx-filename-extension":OFF,
+    "style-prop-object":OFF,
+    "prefer-destructuring":OFF,
+    "no-unused-vars":OFF,
+    "no-tabs":"off",
+    "no-dupe-keys":OFF,
+    "eqeqeq":OFF,
+    'no-console': OFF,
     'no-continue': OFF,
+    'no-shadow': OFF,
     'consistent-return': OFF,
     'no-cond-assign': OFF,
     'no-plusplus': OFF,
@@ -21,7 +38,7 @@ module.exports = {
     'import/no-extraneous-dependencies': OFF,
     'import/extensions': OFF,
     'import/no-unresolved': OFF,
-    'import/no-dynamic-require': OFF
+    'import/prefer-default-export': OFF
   },
   env: {
     node: true,
